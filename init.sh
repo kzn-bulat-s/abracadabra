c1b352f0acf014bbc6c20cb778eef9739e8e594d
boo <boo@boo.boo>
2017-01-01 23:34:18 +0000

    tidy up files a little bit

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
+git config --global user.name 'boo'
+git config --global user.email 'boo@boo.boo'
+
 rm /etc/nginx.conf
 ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
@@ -5,6 +8,5 @@ ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
 
 cd /home/box/web
-
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:app
+sudo gunicorn -c etc/hello.py hello:app
 
