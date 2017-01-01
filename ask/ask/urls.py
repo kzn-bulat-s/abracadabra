e12c856d9964250e0182d1f8b53e9745a741ccfc
Bulat Salyakhutdinov <kzn.bulat.s@gmail.com>
2017-01-02 02:40:00 +0300

    ask/urls.py fixed

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
     # Examples:
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include('qa'),
+    url(r'', include('qa.urls')),
 )
