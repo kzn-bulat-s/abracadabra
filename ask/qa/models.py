34380ceed5c2daead1ff728d19a85b2b76fcd9e5
boo <boo@boo.boo>
2017-01-04 17:14:13 +0000

    set up models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import models as auth_models
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return all().order_by('-added_at')
+
+    def popular(self):
+        return all().order_by('-rating')
+
+class Question(models.Model):
+    objects = QuestionManager()
+
+    title = models.CharField(max_length=140)
+    text = models.TextField()
+    added_at = models.DateField()
+    rating = models.IntegerField()
+    author = models.ForeignKey(auth_models.User)
+    likes = models.ManyToManyField(auth_models.User)
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField()
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(auth_models.User)
+
--- init.sh
+++ init.sh
@@ -1,9 +1,16 @@
 git config --global user.name 'boo'
 git config --global user.email 'boo@boo.boo'
 
+/etc/init.d/mysql restart
+
+mysql -u root </home/box/web/etc/mysql.conf
+
+
+sudo killall gunicorn
 cd /home/box/web
 gunicorn -b 0.0.0.0:8080 hello:app &
 cd ask
+python manage.py syncdb
 gunicorn -b 0.0.0.0:8000 ask.wsgi &
 
 rm /etc/nginx/nginx.conf
