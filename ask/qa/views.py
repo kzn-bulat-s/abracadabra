6031eceaa41385d38247bf44038244d7e670c8a4
boo <boo@boo.boo>
2017-01-05 14:39:09 +0000

    qa/views stub added

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,5 +1,12 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_or_404
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
+
+def page(request, page_no):
+    pass
+
+def popular(request, page_no):
+    pass
+
+
